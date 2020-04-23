2eef86448fa760eca39d67af4757feac4c43d442
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-04-23 15:02:21 -0500

    commit

--- js/date.js
+++ js/date.js
@@ -1,4 +1,37 @@
+// current date 1
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December"
+];
 
+let d = new Date();
+let dayName = dayname[d.getDay()];
+let monthName = months[d-getMonth()];
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate;
+
+
+// current date 2
 try {
 let options = {
     weekday: "long",
