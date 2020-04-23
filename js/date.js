85460b1f0d597b0da949b9ab4f0aba770ee79aa5
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-04-23 15:24:34 -0500

    commit

--- js/date.js
+++ js/date.js
@@ -35,14 +35,15 @@ document.getElementById("currentdate").textContent = fulldate;
 
 try {
     let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric"
     };
     document.getElementById(
-        "currentdate2"
+      "currentdate2"
     ).textContent = new Date().toLocaleDateString("en-US", options);
-    } catch (e) {
-        alert("Error with code or your browser does not support Locale);
-    }
+  } catch (e) {
+    alert("Error with code or your browser does not support Locale");
+  }
+  
