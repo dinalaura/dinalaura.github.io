6c78d3c88fe0e0ed54e6b340295172c2662aad42
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-05-15 02:39:32 -0500

    commit

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,6 +1,6 @@
 //var string = document.lastModified;
 //document.getElementById("date").textContent = string;
-
+/*
 try{
     let options = {
         weekday: "long",
@@ -12,3 +12,35 @@ try{
 } catch (e) {
     alert ("Error with code or your browser does not support Locale");
 }
+*/
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + d.getFullYear();
+
+document.getElementById("date").textContent = fulldate;
