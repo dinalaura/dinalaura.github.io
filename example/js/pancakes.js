c8751d215f7c0e39cc20eefadbd7953a0b3173e3
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-05-24 02:53:20 -0500

    commit

--- example/js/pancakes.js
+++
@@ -1,8 +0,0 @@
-let d = new Date()
-const banner = document.getElementById('banner');
-if(d.getDay() == 5){
-  banner.style.display = 'block';
-}
-else{
-  banner.style.display = 'none';
-}
--- example/js/year.js
+++
@@ -1,2 +0,0 @@
-var string = new Date().getFullYear();
-document.getElementById("year").textContent = string;
