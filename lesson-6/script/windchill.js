524062b5325587f9ff010c7cce97d523320f8f2e
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-05-30 15:59:38 -0500

    commit

--- lesson-6/script/windchill.js
+++ lesson-6/script/windchill.js
@@ -1,6 +1,37 @@
 
 
+/******function doInputOutput(){
+	var tempF = parseFloat(document.getElementById('high').value)
+	var speed = parseFloat(document.getElementById('speed').value);
 
-span id="high"
-id="chill"
-id="speed"
+	var output = windchill(tempF, speed);
+
+	document.getElementById('chill').innerHTML = output;
+	}
+
+
+function windchill(tempF, speed) {
+let t = tempF;
+let s = speed;
+    let f =  35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));    
+
+    return f;
+}
+
+***********/
+
+var tempF = parseFloat(document.getElementById('high').value)
+var speed = parseFloat(document.getElementById('speed').value);
+
+var output = windchill(tempF, speed);
+
+document.getElementById('chill').innerHTML = output;
+
+
+function windchill(tempF, speed) {
+let t = tempF;
+let s = speed;
+    let f =  35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));    
+
+    return f;
+}
