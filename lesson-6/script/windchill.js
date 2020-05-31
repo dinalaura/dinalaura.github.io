7ea8881c1970645cbd2bfdcc1216657e0fd2ea15
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-05-30 19:06:49 -0500

    commit

--- lesson-6/script/windchill.js
+++ lesson-6/script/windchill.js
@@ -1,40 +1,48 @@
 
 
-/******function doInputOutput(){
+/******
+ 
+ function output(){
 	var tempF = parseFloat(document.getElementById('high').value)
 	var speed = parseFloat(document.getElementById('speed').value);
 
 	var output = windchill(tempF, speed);
 
-	document.getElementById('chill').innerHTML = output;
+	document.getElementById('windchill').innerHTML = output.toFixed(2); 
+;
 	}
 
 
 function windchill(tempF, speed) {
+			if(t <= 50 && s>=3){
 let t = tempF;
 let s = speed;
     let f =  35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));    
 
-    return f;
+	return f;
+			}
 }
 
-***********/
 
-var tempF = parseFloat(document.getElementById('high').value)
-var speed = parseFloat(document.getElementById('speed').value);
+***********/	
+	
+function output(){
+	var tempF = parseFloat(document.getElementById('high').value)
+	var speed = parseFloat(document.getElementById('speed').value);
 
-var output = windchill(tempF, speed);
+	var output = windchill(tempF, speed);
 
-document.getElementById('chill').innerHTML = output;
+	document.getElementById('windchill').innerHTML = output.toFixed(2); 
+;
+	}
 
 
 function windchill(tempF, speed) {
+			if(t <= 50 && s>=3){
 let t = tempF;
 let s = speed;
-if(f<=50 && s>=3){
-	let f =  35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));    
+    let f =  35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));    
 
-    return f;
-}
-   
+	return f;
+			}
 }
