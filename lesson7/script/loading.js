0b7e665aef6d95225059b1ff80ee7452826aa3ff
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-06-15 14:28:00 -0500

    commit

--- lesson7/script/loading.js
+++ lesson7/script/loading.js
@@ -1,11 +1,35 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
+	image.setAttribute('src', image.getAttribute('data-src'));
+	image.onload = () => {
+		image.removeAttribute('data-src');
+	};
+};
+ 
+const imgOptions = {
+	root: document.querySelector('#scrollArea'),
+	rootMargin: '0px',
+	threshold: 0.5
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+if ('IntersectionObserver' in window) {
+
+	const imgObserver = new IntersectionObserver((items, observer) => {
+		items.forEach((item) => {
+			if (item.isIntersecting) {
+				loadImages(item.target);
+				imgObserver.unobserve(item.target);
+			}
+		});
+	}, imgOptions);
+
+	imagesToLoad.forEach((img) => {
+		imgObserver.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach((img) => {
+		loadImages(img);
+	});
+}
