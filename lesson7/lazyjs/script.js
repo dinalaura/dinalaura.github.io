66936c5e6d44269db949f392128f32e1d48b27f5
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-06-15 14:18:44 -0500

    commit

--- lesson7/lazyjs/script.js
+++ lesson7/lazyjs/script.js
@@ -1,12 +1,26 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagestoLoad = document.querySelectorAll("img[data-src]");
+
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+    rootMargin: 0px 0px 50px 0px"
 };
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image,removeAttribute('data-src');};
-};
+ const loadImages = (image) => {
+     image.setAttribute('src', image.getAttribute('data-src'));
+     image.onload = () => {image.removeAttribute('data-src');};
+ };
 
-if('IntersectionObserver')
+ if('IntersectionObserver' in window){
+     const imgObserver = new IntersectionObserver((items, observer) =>{
+         items.forEach((item) => {
+     });
+    }, imgOptions);
+
+    imagestoLoad.forEach((img) => {
+        imgOptions.observe(img);
+    });
+    }
+    else{
+        
+    }
+ }
