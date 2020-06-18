e30fde9042287661e24c78b4ac4d9b169a77b88a
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-06-18 18:13:06 -0500

    commit

--- lesson9/jsprophets/prophets.js
+++ lesson9/jsprophets/prophets.js
@@ -1,20 +1,25 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+.then(function(response) {
+  return response.json();
+})
+.then(function(jsonObject){
+  console.table(jsonObject);
+  const prophets = jsonObject['prophets'];
 
-    const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) {
-        let card = document.createElement('section');
-let h2 = document.createElement('h2');
+  prophets.forEach(prophet => {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+    h2.textContent = prophet.name + ' ' + prophet.lastname;
+    p1.innerHTML = "<strong>" + prophet.birthdate + "</strong>";
 
-card.appendChild(h2);
+    card.appendChild(h2);
+    card.appendChild(p1);
 
-document.querySelector('div.cards').appendChild(card);
+    document.querySelector('div.cards').appendChild(card);
+    
   });
+});
