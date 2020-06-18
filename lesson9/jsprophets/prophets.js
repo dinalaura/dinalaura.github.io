94d1eea4b43650cecca16b51973142e20b6fa315
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-06-18 18:50:46 -0500

    commit

--- lesson9/jsprophets/prophets.js
+++ lesson9/jsprophets/prophets.js
@@ -12,12 +12,22 @@ fetch(requestURL)
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let image = document.createElement('img');
+
+
 
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    p1.innerHTML = `<strong>Date of Birth${prophet.birthdate}</strong>`;
+    p1.innerHTML = `Date of Birth: ${prophet.birthdate}`;
+    p2.innerHTML = `Place of Birth: ${prophet.birthplace}`;
+    image.setAttribute('src', prophet.imageurl);
+
 
     card.appendChild(h2);
     card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(image);
+
 
     document.querySelector('div.cards').appendChild(card);
     
