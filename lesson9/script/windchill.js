2f0f4ba7bbdf5a97bc6d970c169ae477a5315fd8
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-06-17 20:27:34 -0500

    commit

---
+++ lesson9/script/windchill.js
@@ -0,0 +1,27 @@
+function output() {
+    let tempF = parseFloat(document.getElementById("high").textContent);
+    let speed = parseFloat(document.getElementById("speed").textContent);
+
+    let output = windChill(tempF, speed);
+
+    document.getElementById("windchill").textContent = output.toFixed(2);
+}
+
+function windChill(tempF, speed) {
+    if (tempF <= 50 && speed >= 3) {
+        let t = tempF;
+        let s = speed;
+        let f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
+
+        return f;
+
+    }
+    else {
+
+        let na = "N/A";
+        return na;
+    }
+}
+
+output()
+
