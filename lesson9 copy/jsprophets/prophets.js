035295a81c89b4069664476d484f9e04cb06a7f5
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-06-21 22:20:29 -0500

    commit

---
+++ lesson9 copy/jsprophets/prophets.js
@@ -0,0 +1,36 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+.then(function(response) {
+  return response.json();
+})
+.then(function(jsonObject){
+//  console.table(jsonObject);
+  const prophets = jsonObject['prophets'];
+
+  prophets.forEach(prophet => {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let image = document.createElement('img');
+
+
+
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    p1.innerHTML = `Date of Birth: ${prophet.birthdate}`;
+    p2.innerHTML = `Place of Birth: ${prophet.birthplace}`;
+    image.setAttribute('src', prophet.imageurl);
+    image.setAttribute('alt', `${prophet.name} ${prophet.lastname} - ${prophet.order}`);
+
+
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(image);
+
+
+    document.querySelector('div.cards').appendChild(card);
+    
+  });
+});
