fa386aee8063f64ba18a4b6a65bd83dca7326d20
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-06-30 23:35:01 -0500

    commit

---
+++ lesson11/script/date.js
@@ -0,0 +1,44 @@
+/*
+try{
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById("date").textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert ("Error with code or your browser does not support Locale");
+}
+*/
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + d.getFullYear();
+
+document.getElementById("date").textContent = fulldate;
