c42eccc746a0c3cf0a2378e3b384fb2207b76b3d
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-07-04 01:16:39 -0500

    commit

--- lesson11/script/prestonweatherapi.js
+++ lesson11/script/prestonweatherapi.js
@@ -1,4 +1,26 @@
-/*-----------------------Forecast------------------------------------------*/
+/*-----------------------Weather Summary------------------------------------------*/
+const weatherSummary = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=195414e543ad58fb7aea951c77ee178a&units=imperial";
+fetch(weatherSummary)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+
+        const condition = document.querySelector('#condition');
+        const temp = document.querySelector('#temp');
+        const high = document.querySelector('#high');
+        const humidity = document.querySelector('#humidity');
+        const speed = document.querySelector('#speed');
+
+        condition.textContent = `${jsObject.weather[0].main}`;
+        temp.textContent = `${jsObject.main.temp} °F`;
+        high.textContent = `${jsObject.main.temp_max} °F`;
+        humidity.textContent = `${jsObject.main.humidity}%`;
+        speed.textContent = `${jsObject.wind.speed} mph`;
+
+    });
+
+
+    /*-----------------------Forecast------------------------------------------*/
 
 const forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=195414e543ad58fb7aea951c77ee178a&units=imperial";
 
