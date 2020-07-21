2988a3f77d7d399e063482a7b6eb118e42710e98
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-07-21 12:23:16 -0500

    commit

--- finalproject/js/loading.js
+++ finalproject/js/loading.js
@@ -3,7 +3,7 @@ const imagesToLoad = document.querySelectorAll('iframe[src]');
 const loadImages = (iframe) => {
 	iframe.setAttribute('src', iframe.getAttribute('src'));
 	iframe.onload = () => {
-		iframe.setAttribute('src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3737.742790701428!2d-86.97723018507656!3d20.475754786300563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec697a2a774f7a00!2sTerminal%20Puerta%20Maya!5e0!3m2!1ses-419!2smx!4v1595029194319!5m2!1ses-419!2smx');
+		iframe.setAttribute('src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3737.742790701428!2d-86.97723018507656!3d20.475754786300563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec697a2a774f7a00!2sTerminal%20Puerta%20Maya!5e0!3m2!1ses-419!2smx!4v1595029194319!5m2!1ses-419!2smx"');
 	};
 };
  
