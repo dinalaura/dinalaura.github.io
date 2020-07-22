f04e710f009f243954f9e0b400e330e3ba7a84af
dinalaura <dinalaura@Dinas-MacBook-Pro.local>
2020-07-22 14:46:22 -0500

    commit

--- finalproject/js/weather.js
+++ finalproject/js/weather.js
@@ -38,3 +38,43 @@ fetch(forecast)
             day++;
         });
     });
+
+
+
+    /*-------SMAL Current Weather Information -------------*/
+
+fetch(weatherSummary)
+.then((response) => response.json())
+.then((jsObject) => {
+    const condition = document.querySelector('#condition');
+    const temp = document.querySelector('#temp');
+    const humidity = document.querySelector('#humidity');
+
+    condition.textContent = `${jsObject.weather[0].description}`;
+    temp.textContent = `${jsObject.main.temp} °F`;
+    humidity.textContent = `${jsObject.main.humidity}%`;
+});
+
+/*-------SMALL forecasted temperature  -------------*/
+
+fetch(forecast)
+.then((response) => response.json())
+.then((jsObject) => {
+
+    const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('12:00:00'));
+
+
+    const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    let day = 0;
+    fivedayforecast.forEach(forecast => {
+        let d = new Date(forecast.dt_txt);
+        document.getElementById(`temp${day + 1}`).textContent = `${forecast.main.temp} °F`;
+        document.getElementById(`day${day + 1}`).textContent = weekdays[d.getDay()];
+        const imagesrc = 'https://openweathermap.org/img/wn/' + forecast.weather[0].icon + '.png';
+        const desc = forecast.weather[0].description;
+        document.getElementById(`ico${day + 1}`).setAttribute('src', imagesrc);
+        document.getElementById(`ico${day + 1}`).setAttribute('alt', desc);
+        day++;
+    });
+});
